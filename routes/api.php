b7fa429a12214df7de8cfb282d0ad6343218b503
Bobby Gerez <bobby.gerez@yahoo.com>
2026-09-07 17:34:20 +0800

    initialize project

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::post('/me', [AuthController::class, 'me']);
